fb8012c65ef383c24ba4de53ad8742950b6128fa
Pierre Mishra <59590433+PierreMishra@users.noreply.github.com>
2020-11-21 17:26:58 -0500

    delineation for multiple outlets, added message for runnning status of tool and cleaned data folder for providing test examples

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Watershed & Stream Delineation Tool
 
-This tool allows a user to delineate watersheds and streams based on an input Digital Elevation Model (DEM) raster using ArcGIS pro. The tool is built on ArcPy script, an ESRI Python package to extend the functionalities of ArcGIS products. There are two ways in which outlet(s) (pour points) can be used to delineate watersheds and streams: 1) using ALL outlet(s) identified in the DEM raster automatically, 2) using specific outlet(s) defined by the user. To provide outlet(s), the user can use a shapefile containing point features or select point(s) on the map.    
+This tool allows a user to delineate watersheds and streams based on Digital Elevation Model (DEM) raster(s) and outlet points as user input using ArcGIS pro. The tool is built on ArcPy module, an ESRI Python package to extend the functionalities of ArcGIS products. There are two ways in which outlet(s) (pour points) can be used to delineate watersheds and streams: 1) using ALL outlet(s) identified in the DEM raster automatically, 2) using specific outlet(s) defined by the user. To provide outlet(s), the user can use a shapefile containing point features or select point(s) on the map.    
+
+# How to use the tool?
+
+## Inputs
+
+## Outputs
+
+## Run 2 test modes
+
+# How to configure ArcGIS pro to run the tool?
+
+## Parameters
+
+## Symbology
+
+# How are folders organized?
+
+# How the tool works?
+
+Step by step process
+
+# Notes
+
+User can check the status of the tool by clicking on View Details under Messages.
+
+Coordinate reference systems of all inputs, intermediary files and output are changed  based on the first DEM selected in the tool.
+
+# 
--- script/DelineationTool.py
+++ script/DelineationTool.py
@@ -3,31 +3,46 @@
 # import packages
 import arcpy
 import numpy as np
-import os
+import os, sys
 
 # Checking path directory
 abs_path = os.path.abspath(sys.argv[0])
-#dir_name = os.path.dirname(abs_path)
-#os.chdir(dir_name)
+dir_name = os.path.dirname(abs_path)
 
 # Allow arcpy to overwrite outputs
 arcpy.env.overwrite = True
-arcpy.addOutputsToMap = True
 
-# Set environment settings 
-arcpy.env.workspace = "V:\\FinalProject\\Watershed-Stream-Delineation-Tool\\scratch"
+# Set environment settings
+scratch_folder = "scratch"
+workspace_folder = f'{dir_name}\\{scratch_folder}' 
+arcpy.env.workspace = env_dir
 
-# DEM raster input by user (change to user input)
-input_raster = [arcpy.Raster("..\\data\\raster_1.tif"), arcpy.Raster("..\\data\\raster_2.tif"), arcpy.Raster("..\\data\\raster_3.tif"), arcpy.Raster("..\\data\\raster_4.tif")]
-#input_raster = arcpy.GetParameterAsText(0)
+# DEM raster input by user 
+input_raster_locations = arcpy.GetParameterAsText(0)
 
-# if added multiple, then mosaic to raster...
-if len(input_raster) == 1:
-    continue
-else:
-    n = len(input_raster)
-    arcpy.MosaicToNewRaster_management
-    #https://pro.arcgis.com/en/pro-app/tool-reference/data-management/mosaic-to-new-raster.htm
+# If input_raster_locations has a semicolon, it means that user provided multiple rasters
+if ";" not in input_raster_locations:
+    # change file location to raster
+    input_raster = arcpy.Raster(input_raster_locations)
+
+else:    
+    # Split raster files
+    split_raster = input_raster_locations.split(";")
+    
+    # Store coordinate reference system of the first raster to change all of them in same CRS
+    first_raster = arcpy.Raster(split_raster[0])
+    first_raster_CRS = arcpy.Describe(first_raster).spatialReference.factoryCode
+    
+    # Mosaic To New Raster arguments
+    raster_location = "..\\data\\"
+    file_name = "merged_raster.tif"
+    pixel_type = "32_BIT_FLOAT"
+    band_num = 1
+    
+    # Join all the rasters
+    arcpy.AddMessage("Joining all DEM rasters...")
+    input_raster = arcpy.MosaicToNewRaster_management(input_raster_locations, raster_location, file_name, first_raster_CRS, pixel_type, "", band_num)
+    
 
 # Ask user which way they want to supply outlet point(s)
 #choice_outlet = 
@@ -37,63 +52,114 @@ raster_CRS = arcpy.Describe(input_raster).spatialReference.factoryCode
 # later assign it the raster's CRS
 
 # Fill in sinks in DEM
+arcpy.AddMessage("Filling sinks in the DEM raster...")
 fill_raster = arcpy.sa.Fill(input_raster)
 
 # Generate flow direction
+arcpy.AddMessage("Computing flow direction based on DEM raster...")
 flow_direction = arcpy.sa.FlowDirection(fill_raster,"", "", "D8")
 
-### CHOICE 1: HAVING NO OUTLETS SHAPEFILE
-# Delineate watersheds based on outlet points in the raster
-#watershed_raster = arcpy.sa.Basin(flow_direction)
-#delineate stream network as well and dissolve them into single shapefile.
-
-#calculate area so user can filter if they want
-
-### CHOICE 2: USER INPUT OUTLET SHAPEFILE
-
 # Generate flow accumulation based on flow direction
+arcpy.AddMessage("Generating flow accumulation based on flow direction...")
 flow_accumulation = arcpy.sa.FlowAccumulation(flow_direction)
 
 # Outlet shapefile input by user (change to user input)
-#input_outlet = "..\\data\\outlet_feature_class2.shp"
+#input_outlet = "..\\data\\outlet_feature_class3.shp"
 input_outlet = arcpy.GetParameterAsText(1)
 
-# Check CRS of outlet feature class
-arcpy.Describe(input_outlet).spatialReference.name
-#make unique file names
+# Change CRS of outlet feature class
+arcpy.AddMessage("Changing spatial reference of outlet feature layer using DEM raster's Well-Known ID...")
 outlet_proj = arcpy.Project_management(input_outlet, "outlet_projected.shp", raster_CRS)
-arcpy.SetParameterAsText(4, outlet_proj)
+arcpy.SetParameterAsText(7, outlet_proj)
 
+# If more than 1 outlet points are provided, ask user whether to aggregate watersheds into 1 polygon or keep them in separate polygons
+aggregate_watershed = arcpy.GetParameterAsText(2)
+#aggregate_watershed = False
+
+# add a new field which will assign unique integers to each outlet if user does not wants to aggregate
+# if user wants to aggregate watersheds into 1, the field will assign same unique integer to each row
+# arcpy.DeleteField_management(outlet_proj, "AggNum")
+arcpy.AddField_management(outlet_proj, "AggNum", "SHORT")
+field = ['AggNum']
+i = 0
+# Count number of rows in outlet point's attribute table
+num_outlet = arcpy.GetCount_management(outlet_proj).messageCount
+if num_outlet > 1:
+    if aggregate_watershed:
+        with arcpy.da.UpdateCursor(outlet_proj, field) as cursor:
+            for row in cursor:
+                row[0] = i
+                cursor.updateRow(row)
+    else:
+        with arcpy.da.UpdateCursor(outlet_proj, field) as cursor:
+            for row in cursor:
+                row[0] = i
+                cursor.updateRow(row)
+                i += 1
+        
 # Snap outlet points to nearest stream
-snap_tolerance = 50 #snap sensitivity, assign to user
-input_outlet_snapped = arcpy.sa.SnapPourPoint(outlet_proj,flow_accumulation, 100)
+arcpy.AddMessage("Snapping outlet point(s) to nearest raster cell with highest flow accumulation value...")
+snap_tolerance = arcpy.GetParameterAsText(3) #snap sensitivity, assign to user
+input_outlet_snapped = arcpy.sa.SnapPourPoint(outlet_proj, flow_accumulation, snap_tolerance, "AggNum")
+
+# Delete the unique integer field we created in outlet points feature layer
+arcpy.DeleteField_management(outlet_proj, "AggNum")
 
 # Delineate watershed based on outlet point by user input
+arcpy.AddMessage("Delineating watershed based on flow direction and snapped outlet(s)...")
 watershed_raster = arcpy.sa.Watershed(flow_direction, input_outlet_snapped)
 
 # Convert watershed raster to polygon
 # make unique file names
+arcpy.AddMessage("Converting watershed raster to polygon shapefile...")
 watershed_polygon = arcpy.RasterToPolygon_conversion(watershed_raster,"watershed_polygon.shp")
-arcpy.SetParameterAsText(2, watershed_polygon)
+arcpy.SetParameterAsText(5, watershed_polygon)
 
 # Delineate stream network if x number of pixels run into 1 pixel for flow accumulation
 # may be take user input for how much x should be
-stream_net = arcpy.sa.Con(flow_accumulation > 1000, flow_accumulation)
+arcpy.AddMessage("Delineating stream network based on user-specified inflow cells...")
+stream_net_tolerance = arcpy.GetParameterAsText(4)
+where_clause = f"VALUE > {int(stream_net_tolerance)}"
+stream_net = arcpy.sa.Con(flow_accumulation, flow_accumulation, "", where_clause)
 
 # Assign stream order for better visuals
+arcpy.AddMessage("Assigning stream order to each stream...")
 stream_order = arcpy.sa.StreamOrder(stream_net, flow_direction)
 
 # Convert raster of stream order to features
-# make unique file names 
+arcpy.AddMessage("Converting stream order rasters to feature layer...") 
 stream_feature = arcpy.sa.StreamToFeature(stream_order, flow_direction, "feature_stream.shp")
 
 # Clip stream network to watershed area
+arcpy.AddMessage("Clip stream network using watershed boundary...")
 stream_feature_clipped = arcpy.Clip_analysis(stream_feature, watershed_polygon, "feature_stream_clipped.shp")
-arcpy.SetParameterAsText(3, stream_feature_clipped)
+arcpy.SetParameterAsText(6, stream_feature_clipped)
+
+# Finished
+arcpy.AddMessage("Watershed(s) and stream delineation finished!")
 
 
 
+                        # ----- X ----- #
+                        
+                        
+                        
+                        
+#-----------------------------------------------------------------------------#
 
+# Trial and error code
+# input_raster = [arcpy.Raster("..\\data\\raster_1.tif"), arcpy.Raster("..\\data\\raster_2.tif"), arcpy.Raster("..\\data\\raster_3.tif"), arcpy.Raster("..\\data\\raster_4.tif")]
+#    n = len(input_raster)
+#    raster_names = ""
+#    for i in input_raster:
+#        # Concatenate raster names into a required format for merging all the rasters using Mosaic to New Raster
+#        print(i)
+#        semicolon = raster_location + i.name + ";"
+#        raster_names = raster_names + semicolon
+### CHOICE 1: HAVING NO OUTLETS SHAPEFILE
+# Delineate watersheds based on outlet points in the raster
+#watershed_raster = arcpy.sa.Basin(flow_direction)
+#delineate stream network as well and dissolve them into single shapefile.
 # Apply symbology to watershed polygon, stream network and outlet point
 #watershed_symbology = "..\\script\\watershed_polygon.lyrx" 
 #stream_symbology = "..\\script\\feature_stream_clipped.lyrx"
