7c1f0d5c985ed1e202aac76a1c2c88fefdb202cc
Pierre Mishra <59590433+PierreMishra@users.noreply.github.com>
2020-11-21 02:26:32 -0500

    Delineated watershed for 1 DEM and 1 pour point, applied symbology, defined input parameters

--- script/DelineationTool.py
+++ script/DelineationTool.py
@@ -12,13 +12,25 @@ abs_path = os.path.abspath(sys.argv[0])
 
 # Allow arcpy to overwrite outputs
 arcpy.env.overwrite = True
+arcpy.addOutputsToMap = True
 
 # Set environment settings 
 arcpy.env.workspace = "V:\\FinalProject\\Watershed-Stream-Delineation-Tool\\scratch"
 
 # DEM raster input by user (change to user input)
-input_raster = arcpy.Raster("..\\data\\wake_county_e5_subset2.tif")
+input_raster = [arcpy.Raster("..\\data\\raster_1.tif"), arcpy.Raster("..\\data\\raster_2.tif"), arcpy.Raster("..\\data\\raster_3.tif"), arcpy.Raster("..\\data\\raster_4.tif")]
+#input_raster = arcpy.GetParameterAsText(0)
+
 # if added multiple, then mosaic to raster...
+if len(input_raster) == 1:
+    continue
+else:
+    n = len(input_raster)
+    arcpy.MosaicToNewRaster_management
+    #https://pro.arcgis.com/en/pro-app/tool-reference/data-management/mosaic-to-new-raster.htm
+
+# Ask user which way they want to supply outlet point(s)
+#choice_outlet = 
 
 # Check CRS of the DEM raster using its Well Known ID
 raster_CRS = arcpy.Describe(input_raster).spatialReference.factoryCode
@@ -32,7 +44,7 @@ flow_direction = arcpy.sa.FlowDirection(fill_raster,"", "", "D8")
 
 ### CHOICE 1: HAVING NO OUTLETS SHAPEFILE
 # Delineate watersheds based on outlet points in the raster
-watershed_raster = arcpy.sa.Basin(flow_direction)
+#watershed_raster = arcpy.sa.Basin(flow_direction)
 #delineate stream network as well and dissolve them into single shapefile.
 
 #calculate area so user can filter if they want
@@ -43,12 +55,14 @@ watershed_raster = arcpy.sa.Basin(flow_direction)
 flow_accumulation = arcpy.sa.FlowAccumulation(flow_direction)
 
 # Outlet shapefile input by user (change to user input)
-input_outlet = "..\\data\\outlet_feature_class2.shp"
+#input_outlet = "..\\data\\outlet_feature_class2.shp"
+input_outlet = arcpy.GetParameterAsText(1)
 
 # Check CRS of outlet feature class
 arcpy.Describe(input_outlet).spatialReference.name
 #make unique file names
 outlet_proj = arcpy.Project_management(input_outlet, "outlet_projected.shp", raster_CRS)
+arcpy.SetParameterAsText(4, outlet_proj)
 
 # Snap outlet points to nearest stream
 snap_tolerance = 50 #snap sensitivity, assign to user
@@ -60,6 +74,7 @@ watershed_raster = arcpy.sa.Watershed(flow_direction, input_outlet_snapped)
 # Convert watershed raster to polygon
 # make unique file names
 watershed_polygon = arcpy.RasterToPolygon_conversion(watershed_raster,"watershed_polygon.shp")
+arcpy.SetParameterAsText(2, watershed_polygon)
 
 # Delineate stream network if x number of pixels run into 1 pixel for flow accumulation
 # may be take user input for how much x should be
@@ -72,10 +87,27 @@ stream_order = arcpy.sa.StreamOrder(stream_net, flow_direction)
 # make unique file names 
 stream_feature = arcpy.sa.StreamToFeature(stream_order, flow_direction, "feature_stream.shp")
 
-# Dissolve stream network to watershed area
+# Clip stream network to watershed area
+stream_feature_clipped = arcpy.Clip_analysis(stream_feature, watershed_polygon, "feature_stream_clipped.shp")
+arcpy.SetParameterAsText(3, stream_feature_clipped)
 
 
 
+
+# Apply symbology to watershed polygon, stream network and outlet point
+#watershed_symbology = "..\\script\\watershed_polygon.lyrx" 
+#stream_symbology = "..\\script\\feature_stream_clipped.lyrx"
+#outlet_symbology = "..\\script\\outlet_projected.lyrx"
+
+#arcpy.ApplySymbologyFromLayer_management(watershed_polygon, watershed_symbology)
+#arcpy.ApplySymbologyFromLayer_management(stream_feature_clipped, stream_symbology)
+#arcpy.ApplySymbologyFromLayer_management(outlet_proj, outlet_symbology) 
+
+#m = arcpy.mp.ArcGISProject("CURRENT")
+#lm = m.listMaps()[0]
+#lm.addLayer(watershed_polygon)
+#lm.addLayer(stream_feature_clipped)
+#lm.addLayer(outlet_proj)
 
 # delete intermediary files?? ask user
 
