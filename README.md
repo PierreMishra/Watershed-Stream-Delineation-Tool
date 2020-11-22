74398d005af7d016bd2ab9da13ead84b9fbc9cfa
Pierre Mishra <59590433+PierreMishra@users.noreply.github.com>
2020-11-21 23:36:46 -0500

    fixed relative path directory issues

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
 # Watershed & Stream Delineation Tool
 
-This tool allows a user to delineate watersheds and streams using Digital Elevation Model (DEM) raster(s) and outlet point(s) provided by a user input in ArcGIS pro. The tool is built on ArcPy module, an ESRI Python package to extend the functionalities of ArcGIS products. It is built for hydrologists, researchers and scientists who work with watershed modeling, hydrology and engineering projects.
+This tool allows a user to delineate watersheds and streams using Digital Elevation Model (DEM) raster(s) and outlet point(s) provided by as an input in ArcGIS pro. The tool is built on ArcPy module, an ESRI Python package to extend the functionalities of ArcGIS products. It is suitable for hydrologists, researchers and scientists working with watershed, hydrologic and engineering projects and studies.
 
-There are two ways in which outlet(s) (pour points) can be used to delineate watersheds and streams: 1) using ALL outlet(s) identified in the DEM raster automatically, 2) using specific outlet(s) defined by the user. To provide outlet(s), the user can use a shapefile containing point features or select point(s) on the map.    
+There are two ways in which outlet(s) (pour points) can be provided to delineate watersheds and streams: 1) user can load their own shapefile containing point feature(s) or 2) user can select point(s) on any base map (for reference) using the pencil tool next to drop down button.    
 
 # How to use the tool?
 
-1. Open ```Watershed-Stream-Delineation.aprx``` and from catalog pane expand ```Watershed-Stream-Delineation.tbx``` under ```Toolboxes```. `hello`  
+Go to this [link](https://1drv.ms/u/s!Ak8N6yOD29xEqT4ax-ypDd4LoWWj?e=NgI3S1) to download the zipped workspace.
+
+Open `Watershed-Stream-Delineation.aprx`. From catalog pane expand `Watershed-Stream-Delineation.tbx` under `Toolboxes`. Double click `Watershed Stream Delineation Tool`.  
 
 ## Inputs
+
+##### Elevation raster(s)
+
+Select a single or multiple Digital Elevation Model (DEM) raster(s) of the geographical area you are interested. Multiple rasters are automatically joined to create a single raster. DEMs should be located from your local drive and file path should not contain any spaces to avoid error. 
+
+A few example DEMs are provided in the `data` folder for testing purpose. The folder `test_1` has a single DEM from an area in Wake County, NC. The folder `test_2` has 4 DEMs that geographically align next to each other. `test_2` files can be used to test inputting multiple rasters. 
+
+More DEMs can be found at USGS's [National Map](https://viewer.nationalmap.gov/basic/#/) service 
+
+
+##### Outlet(s)
+
+There are two ways to load outlet points. 
+1. Load a shapefile containing point feature(s) from your local drive.
+2. 
+*Note - Outlet(s) should be near point
 
 ## Outputs
 
 ## Run 2 test modes
 
 # How to configure ArcGIS pro to run the tool?
+
+This section is for users who are only interested in using the .py script in their projects. 
 
 ## Parameters
 
--- script/DelineationTool.py
+++ script/DelineationTool.py
@@ -1,21 +1,23 @@
-# add message while each process runs
-
-# import packages
+# Import modules
 import arcpy
 import numpy as np
 import os, sys
 
 # Checking path directory
+# When running through ArcGIS pro, abs_path refers to this script
+# so I used os.path.dirname twice to go the parent folder of this project
+# and then use relative path for setting up project's workspace in ArcGIS pro
 abs_path = os.path.abspath(sys.argv[0])
-dir_name = os.path.dirname(abs_path)
+dir_name = os.path.dirname(os.path.dirname(abs_path))
 
 # Allow arcpy to overwrite outputs
 arcpy.env.overwrite = True
+arcpy.AddToDisplay = True
 
 # Set environment settings
 scratch_folder = "scratch"
 workspace_folder = f'{dir_name}\\{scratch_folder}' 
-arcpy.env.workspace = env_dir
+arcpy.env.workspace = workspace_folder
 
 # DEM raster input by user 
 input_raster_locations = arcpy.GetParameterAsText(0)
@@ -29,12 +31,12 @@ else:
     # Split raster files
     split_raster = input_raster_locations.split(";")
     
-    # Store coordinate reference system of the first raster to change all of them in same CRS
+    # Store coordinate reference system of the first raster and change all of them to the same CRS
     first_raster = arcpy.Raster(split_raster[0])
     first_raster_CRS = arcpy.Describe(first_raster).spatialReference.factoryCode
     
-    # Mosaic To New Raster arguments
-    raster_location = "..\\data\\"
+    # Arguments for Mosaic To New Raster geoprocessing tool
+    raster_location = f'{dir_name}\\data\\'
     file_name = "merged_raster.tif"
     pixel_type = "32_BIT_FLOAT"
     band_num = 1
@@ -42,14 +44,10 @@ else:
     # Join all the rasters
     arcpy.AddMessage("Joining all DEM rasters...")
     input_raster = arcpy.MosaicToNewRaster_management(input_raster_locations, raster_location, file_name, first_raster_CRS, pixel_type, "", band_num)
-    
-
-# Ask user which way they want to supply outlet point(s)
-#choice_outlet = 
 
 # Check CRS of the DEM raster using its Well Known ID
 raster_CRS = arcpy.Describe(input_raster).spatialReference.factoryCode
-# later assign it the raster's CRS
+arcpy.env.outputCoordinateSystem = raster_CRS # to avoid an error in Fill process
 
 # Fill in sinks in DEM
 arcpy.AddMessage("Filling sinks in the DEM raster...")
@@ -76,28 +74,37 @@ arcpy.SetParameterAsText(7, outlet_proj)
 aggregate_watershed = arcpy.GetParameterAsText(2)
 #aggregate_watershed = False
 
-# add a new field which will assign unique integers to each outlet if user does not wants to aggregate
-# if user wants to aggregate watersheds into 1, the field will assign same unique integer to each row
-# arcpy.DeleteField_management(outlet_proj, "AggNum")
+# Add a new field to determine how to handle multiple watersheds from multiple outlets  
+arcpy.DeleteField_management(outlet_proj, "AggNum")
 arcpy.AddField_management(outlet_proj, "AggNum", "SHORT")
 field = ['AggNum']
 i = 0
+
 # Count number of rows in outlet point's attribute table
 num_outlet = arcpy.GetCount_management(outlet_proj).messageCount
+
+# Check if number of outlets are more than 1
 if num_outlet > 1:
-    if aggregate_watershed:
+    
+    # if user checks aggregate watersheds, same integer value i will be added to each row of AggNum field
+    if aggregate_watershed == 'true':
         with arcpy.da.UpdateCursor(outlet_proj, field) as cursor:
             for row in cursor:
                 row[0] = i
                 cursor.updateRow(row)
-    else:
+    
+    # if unchecked, it will assign unique integers to each outlet feature if user does not wants to aggregate
+    elif aggregate_watershed == 'false':
         with arcpy.da.UpdateCursor(outlet_proj, field) as cursor:
             for row in cursor:
                 row[0] = i
                 cursor.updateRow(row)
                 i += 1
-        
-# Snap outlet points to nearest stream
+    # Assigning unique value to each outlet point feature will make snap outlet raster of different values
+    # in the next step which will make separate watersheds 
+
+# Snap outlet points to nearest stream pixel, outputs 1 pixel for each outlet point overlapping the pixel
+# highest flow accumulation within a specified proximity
 arcpy.AddMessage("Snapping outlet point(s) to nearest raster cell with highest flow accumulation value...")
 snap_tolerance = arcpy.GetParameterAsText(3) #snap sensitivity, assign to user
 input_outlet_snapped = arcpy.sa.SnapPourPoint(outlet_proj, flow_accumulation, snap_tolerance, "AggNum")
@@ -111,7 +118,7 @@ watershed_raster = arcpy.sa.Watershed(flow_direction, input_outlet_snapped)
 
 # Convert watershed raster to polygon
 # make unique file names
-arcpy.AddMessage("Converting watershed raster to polygon shapefile...")
+arcpy.AddMessage("Converting watershed raster to polygon feature...")
 watershed_polygon = arcpy.RasterToPolygon_conversion(watershed_raster,"watershed_polygon.shp")
 arcpy.SetParameterAsText(5, watershed_polygon)
 
